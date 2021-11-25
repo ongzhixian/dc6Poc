b118ab6eef3ed813d072789821ebdb10d4bd277d
Zhixian Ong <zhixian@hotmail.com>
2021-11-25 17:02:10 +0800

    Update

--- Dn6Poc.TravalApi/Program.cs
+++ Dn6Poc.TravalApi/Program.cs
@@ -2,7 +2,14 @@ using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSingleton<GreetingService>(new GreetingService());
+// Code examples of reading configuration from the appsettings.json file and user-secrets
+// var movieApiKey = builder.Configuration["Movies__ServiceApiKey2"];
+// var author = builder.Configuration["Application:Author"];
+// Console.WriteLine("Author: " + author);
+
+// Setup dependencies for injection
+// builder.Services.AddSingleton<GreetingService>(new GreetingService(builder.Configuration));
+builder.Services.AddSingleton<GreetingService>();
 
 var app = builder.Build();
 
--- Dn6Poc.TravalApi/Services/GreetingService.cs
+++ Dn6Poc.TravalApi/Services/GreetingService.cs
@@ -1,7 +1,14 @@
 public class GreetingService
 {
+    private readonly IConfiguration _configuration;
+
+    public GreetingService(IConfiguration configuration)
+    {
+        _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+    }
+
     public string SayHello(string name)
     {
-        return $"Hello {name}";
+        return $"Hello {name} from {_configuration["Application:Author"]}";
     }
 }
