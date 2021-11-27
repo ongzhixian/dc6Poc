dec980153ecb9444ed40e7f110f1d21698d6dfb9
zhixian <zhixian@hotmail.com>
2021-11-27 12:35:57 +0800

    add serilog logging

--- Dn6Poc.TravalApi/Program.cs
+++ Dn6Poc.TravalApi/Program.cs
@@ -1,19 +1,20 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 // Code examples for reference of reading configuration from the appsettings.json file and user-secrets
+// var safeTravelConnectionString = builder.Configuration["ConnectionStrings:SafeTravel"];
+// Console.WriteLine($"SafeTravel connectionString is: [{safeTravelConnectionString}]");
 
-var safeTravelConnectionString = builder.Configuration["ConnectionStrings:SafeTravel"];
-Console.WriteLine($"SafeTravel connectionString is: [{safeTravelConnectionString}]");
 // var author = builder.Configuration["Application:Author"];
 // Console.WriteLine("Author: " + author);
 // var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 // Console.WriteLine($"Environment is: [{environment}]");
 
-
+////////////////////////////////////////
 // Setup dependencies for injection
 
 // Works
@@ -25,13 +26,28 @@ Console.WriteLine($"SafeTravel connectionString is: [{safeTravelConnectionString
 //     );
 
 // Works
+// builder.Services.AddLogging();
 builder.Services.AddDbContext<Dn6Poc.TravalApi.DbContexts.SafeTravelContext>();
-builder.Services.AddSingleton<GreetingService>();
+builder.Services.AddScoped<GreetingService>();
 // --OR--
 // builder.Services.AddSingleton<GreetingService>(new GreetingService(builder.Configuration));
 
 
+// ZX: Probably don't need this for now; comment out
+// string executingDirectory = executingDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) ?? string.Empty;
+// builder.Host.UseContentRoot(executingDirectory);
+
+
+builder.Host.UseSerilog((hostBuilderContext, loggerConfiguration) =>
+{
+    loggerConfiguration.ReadFrom.Configuration(hostBuilderContext.Configuration);
+});
+
 var app = builder.Build();
+
+
+////////////////////////////////////////
+// Main Script
 
 if (app.Environment.IsDevelopment())
 {
@@ -42,9 +58,35 @@ else
     Console.WriteLine("Other  environment");
 }
 
+////////////////////////////////////////
+// Setup routes
 
 app.MapGet("/", () => "Hello World!");
 
 app.MapGet("/hello", (HttpContext context, GreetingService greetingService) => greetingService.SayHello(context.Request.Query["name"].ToString()));
 
+ILogger log = app.Services.GetRequiredService<ILogger<Program>>();
+
+log.LogInformation("Application start");
+
 app.Run();
+
+// using (ILoggerFactory loggerFactory = new LoggerFactory())
+// using (IHost host = CreateHostBuilder(args).Build())
+// {
+//     IServiceProvider services = host.Services;
+
+//     ILogger log = services.GetRequiredService<ILogger<Program>>();
+
+//     log.LogInformation("Application start");
+
+//     try
+//     {
+//         await host.RunAsync();
+//     }
+//     catch (Exception ex)
+//     {
+//         log.LogError(ex, string.Empty);
+//         throw;
+//     }
+// }
