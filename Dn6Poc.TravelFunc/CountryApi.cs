b423e1ab9c68dcc816b0e23ca9e2243bb146b2c5
Zhixian Ong <zhixian@hotmail.com>
2021-12-08 14:38:44 +0800

    Add travel functions

--- Dn6Poc.TravelFunc/CountryApi.cs
+++ Dn6Poc.TravelFunc/CountryApi.cs
@@ -3,6 +3,10 @@ using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Dn6Poc.CountryApi.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Threading.Tasks;
 
 namespace Dn6Poc.TravelFunc
 {
@@ -10,22 +14,113 @@ namespace Dn6Poc.TravelFunc
     {
         private readonly ILogger _logger;
 
+        private readonly IMongoCollection<Country> _countries;
+
         public CountryApi(ILoggerFactory loggerFactory)
         {
             _logger = loggerFactory.CreateLogger<CountryApi>();
+
+                
+            var client = new MongoClient(System.Environment.GetEnvironmentVariable("mongoDb:safeTravel"));
+            var database = client.GetDatabase("safe_travel");
+            _countries = database.GetCollection<Country>("country");
+
+            _logger.LogInformation("CountryService created");
+
         }
 
-        [Function("Country")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        [Function("CountryExample")]
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "Example")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            
+            var query = System.Web.HttpUtility.ParseQueryString(req.Url.Query);
+            string startswith = query["startswith"];
 
-            response.WriteString("Welcome to Azure Functions!");
+            _logger.LogInformation($"GetCountriesAsync called; startswith: [{startswith}]");
+
+            var filter = Builders<Country>.Filter.Regex(x => x.Name, new BsonRegularExpression($"^{startswith}", "i"));
+
+            ProjectionDefinition<Country> projection = "{ id: 0 }";
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+
+            response.Headers.Add("Content-Type", "application/json; charset=utf-8");
+
+            // Synchronous
+            // response.WriteString(
+            //     System.Text.Json.JsonSerializer.Serialize(
+            //         _countries.Find(filter).Project<Country>(projection).ToList()));
+
+
+            // Asynchronous
+            var cursor = await _countries.FindAsync<Country>(filter, new FindOptions<Country, Country>() { Projection = projection });
+
+            var result = await cursor.ToListAsync();
+
+            response.WriteString(
+                System.Text.Json.JsonSerializer.Serialize(
+                    result
+            ));
 
             return response;
+
+        }
+
+        [Function("ListCountry")]
+        public async Task<HttpResponseData> List(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route ="country")] 
+            HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var query = System.Web.HttpUtility.ParseQueryString(req.Url.Query);
+            string startswith = query["startswith"];
+
+            _logger.LogInformation($"GetCountriesAsync called; startswith: [{startswith}]");
+
+            var filter = Builders<Country>.Filter.Regex(x => x.Name, new BsonRegularExpression($"^{startswith}", "i"));
+
+            ProjectionDefinition<Country> projection = "{ id: 0 }";
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            
+            var cursor = await _countries.FindAsync<Country>(filter, new FindOptions<Country, Country>() { Projection = projection });
+
+            var result = await cursor.ToListAsync();
+
+            await response.WriteAsJsonAsync(result);
+
+            return response;
+
+        }
+
+
+        [Function("GetCountry")]
+        public async Task<HttpResponseData> GetCountry(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route ="country/{id}")] 
+            HttpRequestData req, 
+            string id)
+        {
+            _logger.LogInformation("GetCountry - C# HTTP trigger function processed a request.");
+
+            _logger.LogInformation($"GetCountry- GetCountriesAsync called; equals: [{id}]");
+
+            var filter = Builders<Country>.Filter.Regex(x => x.Name, new BsonRegularExpression($"^{id}$", "i"));
+            
+            ProjectionDefinition<Country> projection = "{ id: 0 }";
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            
+            var cursor = await _countries.FindAsync<Country>(filter, new FindOptions<Country, Country>() { Projection = projection });
+
+            var result = await cursor.ToListAsync();
+
+            await response.WriteAsJsonAsync(result);
+
+            return response;
+
         }
     }
 }
---
+++ Dn6Poc.TravelFunc/Models/Country.cs
@@ -0,0 +1,17 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Dn6Poc.CountryApi.Models;
+
+public partial class Country
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    [BsonElement("name")]
+    public string Name { get; set; }
+
+    [BsonElement("code")]
+    public string Code { get; set; }
+}
