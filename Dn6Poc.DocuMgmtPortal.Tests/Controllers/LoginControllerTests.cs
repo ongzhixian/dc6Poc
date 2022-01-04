759d49b7d132b851e635e8067fa33b62a73718ba
Zhixian Ong <zhixian@hotmail.com>
2022-01-04 16:51:12 +0800

    add dummy test

---
+++ Dn6Poc.DocuMgmtPortal.Tests/Controllers/LoginControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dn6Poc.DocuMgmtPortal.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dn6Poc.DocuMgmtPortal.Controllers.Tests
+{
+    [TestClass()]
+    public class LoginControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
